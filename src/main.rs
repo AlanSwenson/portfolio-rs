92678610b7b48c228d670cc1d7606a8751f42f0c
AlanSwenson <alan@alanswenson.dev>
2021-02-12 11:59:24 -0600

    add tera templating

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let context: HashMap<&str, &str> = [("name", "Alan")].iter().cloned().collect();
+    Template::render("index", &context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
