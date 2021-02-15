478ab027382d41c8633a47b7d873468ccfd84392
AlanSwenson <alan@alanswenson.dev>
2021-02-15 17:32:10 -0600

    add dynamic date to footer

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,20 @@
 #[macro_use]
 extern crate rocket;
 
+use chrono::{Datelike, Utc};
 use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
 use std::collections::HashMap;
 
 #[get("/")]
 fn index() -> Template {
-    let context: HashMap<&str, &str> = [("name", "Alan Swenson")].iter().cloned().collect();
+    let now = Utc::now();
+    let (_is_common_era, year) = now.year_ce();
+    let borrowed_year = year.to_string();
+    let context: HashMap<&str, &str> = [("name", "Alan Swenson"), ("year", &borrowed_year)]
+        .iter()
+        .cloned()
+        .collect();
     Template::render("main", &context)
 }
 
